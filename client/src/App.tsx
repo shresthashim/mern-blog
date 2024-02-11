fb400362379fc17b13d2f06f02ca7fe3d1392972
AsHim Shrestha <ashimsth89@gmail.com>
2024-02-11 19:06:00 +0545

    feat : create route paths using browse router

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,28 @@
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import Home from "./pages/Home.tsx";
+import About from "./pages/About.tsx";
+import SignIn from "./pages/SignIn.tsx";
+import SignUp from "./pages/SignUp.tsx";
+import Projects from "./pages/Projects.tsx";
+import Dashboard from "./pages/Dashboard.tsx";
 
 function App() {
 
   return (
-    <>
-        <div className='text-amber-300'>Blog</div>
-    </>
+    <BrowserRouter>
+
+        <Routes>
+            <Route path="/" element={<Home />} />
+
+            <Route path={'/about'} element={<About />} />
+            <Route path={'/sign-in'} element={<SignIn />} />
+            <Route path={'/sign-up'} element={<SignUp />} />
+            <Route path={'/projects'} element={<Projects />} />
+            <Route path={'/dashboard'} element={<Dashboard />} />
+
+
+        </Routes>
+    </BrowserRouter>
   )
 }
 
