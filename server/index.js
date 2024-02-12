feac2ef3e039438b0484051024d2213532ff604f
AsHim Shrestha <ashimsth89@gmail.com>
2024-02-12 08:33:08 +0545

    successful connection to mongodb

--- server/index.js
+++ server/index.js
@@ -1,6 +1,17 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
 const app = express();
+
+
+mongoose.connect(process.env.MONGO_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(r =>
+  console.log('Connected to MongoDB')
+).catch(e => console.log('Error connecting to MongoDB', e))
 app.listen(`3000`, () => {
   console.log(`Server is running on port 3000`);
 })
