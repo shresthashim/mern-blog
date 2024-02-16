5d678ab875abd2249288640779447337286cd9d0
AsHim Shrestha <ashimsth89@gmail.com>
2024-02-16 18:55:38 +0545

    minor changes

--- client/src/pages/SignUp.tsx
+++ client/src/pages/SignUp.tsx
@@ -10,13 +10,12 @@ export default function SignUp() {
                     <Link to='/' className='font-bold dark:text-white text-4xl'>
             <span
                 className='px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white'>
-              Sahand's
+              AsHim's
             </span>
                         Blog
                     </Link>
                     <p className='text-sm mt-5'>
-                        This is a demo project. You can sign up with your email and password
-                        or with Google.
+                       Welcome to my blog. Please sign up to continue. You can also sign in if you already have an account.
                     </p>
                 </div>
                 {/* right */}
